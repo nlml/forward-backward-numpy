5b56a568d30bf009a4ea6960706bed2e37febf56
liam schoneveld <liam.sch@gmail.com>
2019-12-17 01:23:47 +0100

    add logarithmic version

--- fwd_bwd_log.py
+++ fwd_bwd_log.py
@@ -8,41 +8,48 @@ def rand_nice_probs(shp):
         E /= E.sum(1, keepdims=True)
     return E
 
-def calc_alpha_k_z_k(E, T, alpha_km1):
+def calc_alpha_k_z_k(logE, logT, log_alpha_km1):
     # E shape = [N_possible_current_states]
     # T shape = [N_possible_previous_states, N_possible_current_states]
     # ak1 shp = [N_possible_previous_states]
-    v = (T * alpha_km1[:, None]).sum(0)
-    return (E * v)
+    b = np.min(log_alpha_km1)
+    out = (logT + log_alpha_km1[:, None] + logE[None, :]) - b
+    return np.log(np.exp(out).sum(0)) + b
 
-def forward(E, T, init_state, observations):
-    fwds = [init_state * E[:, observations[0]]]
+def forward(logE, logT, log_init_state, observations):
+    fwds = [log_init_state + logE[:, observations[0]]]
     for i, obs in enumerate(observations[1:]):
-        fwds.append(calc_alpha_k_z_k(E[:, obs], T, fwds[-1]))
+        fwds.append(calc_alpha_k_z_k(logE[:, obs], logT, fwds[-1]))
     return np.array(fwds)
 
-def calc_beta_k_z_k(E, T, beta_kp1):
+def calc_beta_k_z_k(logE, logT, log_beta_kp1):
     # E shape = [N_possible_future_states]
     # T shape = [N_possible_current_states, N_possible_future_states]
     # bk1 shp = [N_possible_future_states]
-    v = (T * beta_kp1[None, :] * E[None, :]).sum(1)
-    return v
+    b = np.min(log_beta_kp1)
+    v = (logT + log_beta_kp1[None, :] + logE[None, :]) - b
+    return np.log(np.exp(v).sum(1)) + b
 
-def backward(E, T, init_state, observations):
-    bwds = [np.ones(T.shape[0])]
+def backward(logE, logT, log_init_state, observations):
+    bwds = [np.zeros(T.shape[0])]  # log(1) == 0
     for i, obs in enumerate(reversed(observations[1:])):
-        bwds.insert(0, calc_beta_k_z_k(E[:, obs], T, bwds[0]))
+        bwds.insert(0, calc_beta_k_z_k(logE[:, obs], logT, bwds[0]))
     return np.array(bwds)
 
 
 def forward_backward(E, T, init_state, observations):
     assert np.all(E.sum(1) == 1)
     assert np.all(T.sum(1) == 1)
-    fwd = forward(E, T, init_state, observations)
-    bak = backward(E, T, init_state, observations)
-    assert np.abs((bak[0] * init_state * E[:, observations[0]])[:2].sum() - fwd[-1][:2].sum()) < 0.0001
-    norm_const = fwd[-1][:2].sum()
-    return (fwd * bak) / norm_const
+    logE = np.log(E)
+    logT = np.log(T)
+    log_init_state = np.log(init_state)
+    fwd = forward(logE, logT, log_init_state, observations)
+    bak = backward(logE, logT, log_init_state, observations)
+    # assert np.abs((bak[0] * init_state * E[:, observations[0]])[:2].sum() - fwd[-1][:2].sum()) < 0.0001
+    b = np.min(fwd[-1])
+    log_norm_const = np.log(np.exp(fwd[-1] - b).sum()) + b
+    # log_norm_const = np.log(np.exp(bak[0] + log_init_state + logE[:, observations[0]]).sum())
+    return np.exp(fwd + bak - log_norm_const)
 
 
 if __name__ == "__main__":
